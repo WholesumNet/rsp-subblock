340aaf72d347ba956066d0b71d6901567465417b
kaiwei-0 <kaiwei@brevis.network>
2025-08-11 21:37:48 +0800

    compile pico aggregator elf

--- bin/client-eth-agg/src/main.rs
+++ bin/client-eth-agg/src/main.rs
@@ -1,5 +1,5 @@
 #![no_main]
-sp1_zkvm::entrypoint!(main);
+pico_sdk::entrypoint!(main);
 
 use reth_primitives::B256;
 use rsp_client_executor::{io::AggregationInput, ClientExecutor, EthereumVariant};
@@ -8,15 +8,15 @@ pub fn main() {
     // Read the input.
     println!("cycle-tracker-start: deserialize");
     // Read the public values, vkey, and aggregation input.
-    let public_values = sp1_zkvm::io::read::<Vec<Vec<u8>>>();
-    let vkey = sp1_zkvm::io::read::<[u32; 8]>();
+    let public_values = pico_sdk::io::read_as::<Vec<Vec<u8>>>();
+    let vkey = pico_sdk::io::read_as::<[u32; 8]>();
     println!("cycle-tracker-start: deserialize aggregation input");
-    let aggregation_input = sp1_zkvm::io::read::<AggregationInput>();
+    let aggregation_input = pico_sdk::io::read_as::<AggregationInput>();
     println!("cycle-tracker-end: deserialize aggregation input");
 
-    let parent_state_root = sp1_zkvm::io::read::<B256>();
-    sp1_zkvm::io::commit(&parent_state_root);
-    sp1_zkvm::io::commit(&aggregation_input.current_block);
+    let parent_state_root = pico_sdk::io::read_as::<B256>();
+    pico_sdk::io::commit(&parent_state_root);
+    pico_sdk::io::commit(&aggregation_input.current_block);
     println!("cycle-tracker-end: deserialize");
 
     let client = ClientExecutor;
@@ -32,5 +32,5 @@ pub fn main() {
 
     let hash = header.hash_slow();
 
-    sp1_zkvm::io::commit(&hash);
+    pico_sdk::io::commit(&hash);
 }
