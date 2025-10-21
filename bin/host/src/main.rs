69fd348ef6cef7433d48c5ba00a5cc19840e9b51
rezahsnz <rezahsnz@proton.me>
2025-10-21 15:55:03 +0330

    feat: write stdin objects to a local Redis instance

--- bin/host/src/main.rs
+++ bin/host/src/main.rs
@@ -127,7 +127,15 @@ async fn schedule_subblock_execution(
     inputs: SubblockHostOutput,
     execute: bool,
     dump_dir: Option<PathBuf>,
-) -> eyre::Result<()> {    
+) -> eyre::Result<()> {
+    // init redis
+    let redis_client = redis::Client::open("redis://127.0.0.1:6379/")?;
+    let mut redis_con = redis_client.get_connection()?;
+    // subblock stream cleanup
+    let _: () = redis::cmd("DEL")
+        .arg("rsp-subblock-stdin-stream")
+        .execute(&mut redis_con);
+
     let (subblock_elf, subblock_vk) = (subblock_pk.elf, subblock_pk.vk);
     let agg_elf = agg_pk.elf;
     
@@ -165,12 +173,25 @@ async fn schedule_subblock_execution(
             std::fs::write(stdin_path, bincode::serialize(&stdin)?)?;
         }
 
+        // write subblock stdin to redis        
+        let _: () = redis::cmd("XADD")
+            .arg("rsp-subblock-stdin-stream")
+            .arg("*")
+            .arg(&[(i.to_string(), bincode::serialize(&stdin)?)])
+            .query(&mut redis_con)?;  
+
         if execute {
             let (_public_values, report) = client.execute(&subblock_elf, &stdin).run().unwrap();
             let subblock_instruction_count = report.total_instruction_count();
             tracing::info!("Subblock {} instruction count: {}", i, subblock_instruction_count);
         }
     }
+    // notify subblock stdin that push is over
+    let _: () = redis::cmd("XADD")
+        .arg("rsp-subblock-stdin-stream")
+        .arg("*")
+        .arg(&[("<done>", "")])
+        .query(&mut redis_con)?;  
 
     if execute {
         // Execute the aggregation program with deferred proof verification off, since we don't have the proof yet.
@@ -182,6 +203,22 @@ async fn schedule_subblock_execution(
         let agg_instruction_count = report.total_instruction_count();
         tracing::info!("Aggregation program instruction count: {}", agg_instruction_count);
     }
+    // write aggregation stdin to redis
+    // agg stream cleanup
+    let _: () = redis::cmd("DEL")
+        .arg("rsp-agg-stdin-stream")
+        .execute(&mut redis_con);        
+    let _: () = redis::cmd("XADD")
+        .arg("rsp-agg-stdin-stream")
+        .arg("*")
+        .arg(&[("0", bincode::serialize(&aggregation_stdin)?)])
+        .query(&mut redis_con)?;        
+    // notify aggregation stdin push is over
+    let _: () = redis::cmd("XADD")
+        .arg("rsp-agg-stdin-stream")
+        .arg("*")
+        .arg(&[("<done>", "")])
+        .query(&mut redis_con)?; 
 
     Ok(())
 }
